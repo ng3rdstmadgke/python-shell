afd117d92bba44cbac86c815f03f35caec575edd
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-08-17 19:51:14 +0900

    ホスト側とのマウントを作成

--- bin/run.sh
+++ bin/run.sh
@@ -12,8 +12,10 @@ python shell を起動する
  -v | --version <VERSION>:
    pythonバージョン指定
    38, 3.8
- --retain:
-   終了と同時にコンテナを削除しない
+ --rm:
+   終了と同時にコンテナを削除
+ --root:
+   rootユーザーで起動
 EOS
 exit 1
 }
@@ -22,12 +24,14 @@ SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 PROJECT_ROOT="$(cd ${SCRIPT_DIR}/..; pwd)"
 
 args=()
-OPTION_REMOVE="--rm"
+OPTIONS=
+CMD="/usr/local/bin/entrypoint.sh"
 while [ "$#" != 0 ]; do
   case $1 in
     -h | --help      ) usage;;
     -v | --version   ) shift;VERSION="$(echo $1 | sed -e 's/[^0-9]//g')";;
-    --retain         ) OPTION_REMOVE="";;
+    --rm             ) OPTIONS="$OPTIONS --rm";;
+    --root           ) CMD="/bin/bash";;
     --               ) shift; args+=($@); break;;
     -* | --*         ) error "$1 : 不正なオプションです" ;;
   esac
@@ -40,8 +44,19 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
+CONTAINER_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n1)
+CONTAINER_NAME=python${VERSION}-shell-$(date +"%Y%m%d.%H%M%S")
+mkdir -p ${PROJECT_ROOT}/mnt/${CONTAINER_NAME}/user1
+mkdir -p ${PROJECT_ROOT}/mnt/${CONTAINER_NAME}/root
+
 docker run \
-  $OPTION_REMOVE \
+  $OPTIONS \
   -ti \
-  --name python${VERSION}-shell \
-  python${VERSION}-shell:latest
+  -e LOCAL_UID=$(id -u) \
+  -e LOCAL_GID=$(id -g) \
+  -v "${PROJECT_ROOT}/mnt/${CONTAINER_NAME}/user1:/mnt/projects" \
+  -v "${PROJECT_ROOT}/mnt/${CONTAINER_NAME}/root:/root/projects" \
+  --name $CONTAINER_NAME \
+  python${VERSION}-shell:latest \
+  $CMD
+
--- docker/python39/entrypoint.sh
+++ docker/python39/entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+set -e
+
 # HOST_UID=${変数名:-デフォルト値}
 HOST_UID=${LOCAL_UID:-1000}
 HOST_GID=${LOCAL_GID:-1000}
 
 # ホスト側の実行ユーザーと同一のUID, GIDを持つユーザーを作成
 echo "Starting with UID : $HOST_UID, GID: $HOST_GID"
-useradd -u $HOST_UID -o -m -U -G sysadmin -s /bin/bash user1
-export HOME=/home/user1
+UNAME="user1"
+groupadd -g $HOST_GID $UNAME
+useradd -u $HOST_UID -o -m -g $HOST_GID -s /bin/bash $UNAME
+usermod -aG sysadmin $UNAME
+export HOME=/home/$UNAME
+
+ln -s /mnt/projects /home/$UNAME/projects
+chown -R $UNAME:$UNAME /mnt/projects /home/$UNAME/projects
+echo 'alias ll="ls -alF"' >> /home/$UNAME/.bashrc
 
 # 作成したユーザーでコマンドを実行
-exec su - user1
+exec su - $UNAME
